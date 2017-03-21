cdd03325b4e23411aafb5ecfc879ad71224c0243
Luiz Gustavo <gustavo.ribeiro.gr10@gmail.com>
2017-03-21 00:03:31 -0300

    Meu Primeiro envio

---
+++ PI.java
@@ -0,0 +1,19 @@
+package pi;
+
+/**
+ *
+ * @author Luiz Gustavo
+ */
+public class PI {
+    
+    public static void main(String[] args) {
+        double f  = 212;
+        
+        double c = (5.0 / 9.0) * (f - 32);
+        
+        System.out.println(f + "°F = " + c + "°C");
+        
+        
+    }
+    
+}
