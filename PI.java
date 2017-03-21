28452e871ab43c01c164bde5effc3f9d5683d0dc
LuizGustavo01 <gustavo.ribeiro.gr10@gmail.com>
2017-03-21 00:25:29 -0300

    Alteração de comentário 2

--- PI.java
+++ PI.java
@@ -9,7 +9,7 @@ public class PI {
     public static void main(String[] args) {
         // ENTRADA
         double f  = 212;
-        
+        // PROCESSAMENTO
         double c = (5.0 / 9.0) * (f - 32);
         
         System.out.println(f + "°F = " + c + "°C");
